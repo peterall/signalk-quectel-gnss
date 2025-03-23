3b760894fa9979daf52798286bf14ed6d76f114e
Peter Allwin <peter@allwin.se>
2025-03-23 17:18:39 +0100

    Only send update if values are valid

--- index.js
+++ index.js
@@ -12,39 +12,52 @@ module.exports = (app) => {
       sentence: 'PQTMTAR',
       parser: ({ id, sentence, parts, tags }, session) => {
         // parts will be <MsgVer>,<Time>,<Quality>,<Res>,<Length>,<Pitch>,<Roll>,<Heading>,<Acc_Pitch>,<Acc_Roll>,<Acc_Heading>,<UsedSV>
-        const baseline = Number(parts[4]);
-        const usedSatellites = Number(parts[11]);
-        const heading = Number(parts[7]);
-        const pitch = Number(parts[5]);
-        const roll = Number(parts[6]);
-        const heading_accuracy = Number(parts[10]);
-        const pitch_accuracy = Number(parts[8]);
-        const roll_accuracy = Number(parts[9]);
-        // measurements assume sensor is mounted longitudinally, however it's mounted transversely
-        // so we need to rotate heading by 90 and normalize it to 0-360, and swap pitch and roll
-        let heading_rad = (heading + 90) * Math.PI / 180;
-        if (heading_rad > Math.PI) {
-          heading_rad -= 2 * Math.PI;
-        }
-        const pitch_rad = roll * Math.PI / 180;
-        const roll_rad = pitch * Math.PI / 180;
-        return {
-          updates: [
-            {
-              values: [
-                { path: 'navigation.headingTrue', value: heading_rad },
-                { path: 'navigation.attitude', value: { roll: roll_rad, pitch: pitch_rad, yaw: heading_rad } },
-                { path: 'navigation.attitudeAccuracy', value: { roll: pitch_accuracy, pitch: roll_accuracy, yaw: heading_accuracy } },
-                { path: 'navigation.attitudeBaseline', value: baseline },
-                { path: 'navigation.attitudeSatellites', value: usedSatellites },
-              ]
-            }
-          ]
+        // $PQTMTAR,1,160443.350,1,,0.000,,,,,,,23*6C
+        const [msg_ver, time, quality, res, length, pitch, roll, heading, acc_pitch, acc_roll, acc_heading, usedsv] = parts;
+
+        if (Number(length) !== 0 && pitch != "" && roll != "" && heading != "") {
+          // measurements assume sensor is mounted longitudinally, however it's mounted transversely
+          // so we need to rotate heading by 90 and normalize it to 0-2pi, and swap pitch and roll
+          let heading_rad = (Number(heading) + 90) * Math.PI / 180;
+          if (heading_rad > Math.PI) {
+            heading_rad -= 2 * Math.PI;
+          }
+
+          const roll_rad = Number(pitch) * Math.PI / 180;
+          const pitch_rad = Number(roll) * Math.PI / 180;
+
+          const acc_roll_rad = Number(acc_pitch) * Math.PI / 180;
+          const acc_pitch_rad = Number(acc_roll) * Math.PI / 180;
+          const acc_yaw_rad = Number(acc_heading) * Math.PI / 180;
+
+          return {
+            updates: [
+              {
+                values: [
+                  { path: 'navigation.headingTrue', value: heading_rad },
+                  { path: 'navigation.attitude', value: { roll: roll_rad, pitch: pitch_rad, yaw: heading_rad } },
+                  { path: 'navigation.attitudeAccuracy', value: { roll: acc_roll_rad, pitch: acc_pitch_rad, yaw: acc_yaw_rad } },
+                  { path: 'navigation.attitudeBaseline', value: Number(length) },
+                  { path: 'navigation.attitudeSatellites', value: Number(usedsv) },
+                ]
+              }
+            ]
+          }
+        } else if (Number(usedsv) > 0) {
+          return {
+            updates: [
+              {
+                values: [
+                  { path: 'navigation.attitudeSatellites', value: Number(usedsv) },
+                ]
+              }
+            ]
+          }
         }
       }
     });
-    app.setPluginStatus('Started');
 
+    app.setPluginStatus('Started');
   };
 
   plugin.stop = () => {
